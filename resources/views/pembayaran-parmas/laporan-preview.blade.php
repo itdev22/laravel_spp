86a1b3bb933df0314bd6814c37582e176d0381a9
itdev22 <90917112+itdev22@users.noreply.github.com>
2023-06-23 01:12:02 +0700

    Update laporan-preview.blade.php

--- resources/views/pembayaran-parmas/laporan-preview.blade.php
+++ resources/views/pembayaran-parmas/laporan-preview.blade.php
@@ -7,27 +7,30 @@
         .container {
             position: relative;
         }
+
         .logo_sma {
             position: absolute;
-            top: 3px; /* Ubah nilai top sesuai dengan posisi vertikal yang diinginkan */
-            left: 1px; /* Ubah nilai left sesuai dengan posisi horizontal yang diinginkan */
+            top: 3px;
+            /* Ubah nilai top sesuai dengan posisi vertikal yang diinginkan */
+            left: 1px;
+            /* Ubah nilai left sesuai dengan posisi horizontal yang diinginkan */
             width: 120px;
 
         }
+
         .logo_dispen {
             position: absolute;
-            top: 5px; /* Ubah nilai top sesuai dengan posisi vertikal yang diinginkan */
-            right: 5px; /* Ubah nilai left sesuai dengan posisi horizontal yang diinginkan */
+            top: 5px;
+            /* Ubah nilai top sesuai dengan posisi vertikal yang diinginkan */
+            right: 5px;
+            /* Ubah nilai left sesuai dengan posisi horizontal yang diinginkan */
             width: 100px;
 
         }
-        h3{
 
-        }
-        h5{
+        h3 {}
 
-        }
-
+        h5 {}
     </style>
 </head>
 
@@ -37,17 +40,22 @@
 
     <div class="container">
         <center>
-        <h3 style="font-family: sans-serif; margin:auto;">PEMERINTAH PROVINSI JAWA TIMUR <br> <b>DINAS PENDIDIKAN <br>
-            SEKOLAH MENENGAH ATAS NEGERI 4 KOTA KEDIRI</b> </h3>
-            <h5 style="padding:0px; margin-top:auto;">JL. Sersan Suharmaji IX/52, Manisrenggo, Telp/Fax. (0354) 688864, KP.64128 Kediri
-            <br> Website : http://.sman4-kdr.sch.id Email : sman4.info@gmail.com</h5><hr>
+            <h3 style="font-family: sans-serif; margin:auto;">PEMERINTAH PROVINSI JAWA TIMUR <br> <b>DINAS PENDIDIKAN
+                    <br>
+                    SEKOLAH MENENGAH ATAS NEGERI 4 KOTA KEDIRI</b> </h3>
+            <h5 style="padding:0px; margin-top:auto;">JL. Sersan Suharmaji IX/52, Manisrenggo, Telp/Fax. (0354) 688864,
+                KP.64128 Kediri
+                <br> Website : http://.sman4-kdr.sch.id Email : sman4.info@gmail.com
+            </h5>
+            <hr>
 
-            <br> <h2 style="font-family: sans-serif;">Laporan Pembayaran Parmas</h2>
+            <br>
+            <h2 style="font-family: sans-serif;">Laporan Pembayaran Parmas</h2>
         </center>
-    <br>
-    <b>Dari tanggal {{ \Carbon\Carbon::parse(request()->tanggal_mulai)->format('d-m-Y') }} -
-        {{ \Carbon\Carbon::parse(request()->tanggal_selesai)->format('d-m-Y') }}</b><br><br>
-    <table style="" border="1" cellspacing="0" cellpadding="10" width="100%">
+        <br>
+        <b>Dari tanggal {{ \Carbon\Carbon::parse(request()->tanggal_mulai)->format('d-m-Y') }} -
+            {{ \Carbon\Carbon::parse(request()->tanggal_selesai)->format('d-m-Y') }}</b><br><br>
+        {{-- <table style="" border="1" cellspacing="0" cellpadding="10" width="100%">
         <thead>
             <tr>
                 <th scope="col" style="font-family: sans-serif;">No</th>
@@ -78,8 +86,8 @@
                 </tr>
             @endforeach
         </tbody>
-    </table>
-</div>
+    </table> --}}
+    </div>
 </body>
 
 </html>
