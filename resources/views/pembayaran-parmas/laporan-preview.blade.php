b124efec0b3a5124d041746ad613c3edaf418f01
dinr19 <dinaromawati1911@gmail.com>
2023-06-23 00:27:11 +0700

    logo

--- resources/views/pembayaran-parmas/laporan-preview.blade.php
+++ resources/views/pembayaran-parmas/laporan-preview.blade.php
@@ -3,13 +3,48 @@
 
 <head>
     <title>GENERATE PDF</title>
+    <style>
+        .container {
+            position: relative;
+        }
+        .logo_sma {
+            position: absolute;
+            top: 3px; /* Ubah nilai top sesuai dengan posisi vertikal yang diinginkan */
+            left: 1px; /* Ubah nilai left sesuai dengan posisi horizontal yang diinginkan */
+            width: 120px;
+
+        }
+        .logo_dispen {
+            position: absolute;
+            top: 5px; /* Ubah nilai top sesuai dengan posisi vertikal yang diinginkan */
+            right: 5px; /* Ubah nilai left sesuai dengan posisi horizontal yang diinginkan */
+            width: 100px;
+
+        }
+        h3{
+            margin:auto;
+        }
+        h5{
+            padding: 0px;
+            margin-top:auto;
+        }
+
+    </style>
 </head>
 
 <body>
-    <br><br>
-    <center>
-        <h2 style="font-family: sans-serif;">Laporan Pembayaran Parmas</h2>
-    </center>
+    <img src="{{ public_path('templates/backend/AdminLTE-3.1.0/dist/img/smapa.png') }}" alt="Images" class="logo_dispen">
+    <img src="{{ public_path('templates/backend/AdminLTE-3.1.0/dist/img/ok.png') }}" alt="Images" class="logo_sma">
+
+    <div class="container">
+        <center>
+        <h3 style="font-family: sans-serif;">PEMERINTAH PROVINSI JAWA TIMUR <br> <b>DINAS PENDIDIKAN <br>
+            SEKOLAH MENENGAH ATAS NEGERI 4 KOTA KEDIRI</b> </h3>
+            <h5>JL. Sersan Suharmaji IX/52, Manisrenggo, Telp/Fax. (0354) 688864, KP.64128 Kediri
+            <br> Website : http://.sman4-kdr.sch.id Email : sman4.info@gmail.com</h5><hr>
+
+            <br> <h2 style="font-family: sans-serif;">Laporan Pembayaran Parmas</h2>
+        </center>
     <br>
     <b>Dari tanggal {{ \Carbon\Carbon::parse(request()->tanggal_mulai)->format('d-m-Y') }} -
         {{ \Carbon\Carbon::parse(request()->tanggal_selesai)->format('d-m-Y') }}</b><br><br>
@@ -45,6 +80,7 @@
             @endforeach
         </tbody>
     </table>
+</div>
 </body>
 
 </html>
