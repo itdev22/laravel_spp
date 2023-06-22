305d0008c98ca92103f648d18103016f63390308
itdev22 <90917112+itdev22@users.noreply.github.com>
2023-06-23 01:18:34 +0700

    Update laporan-preview.blade.php

--- resources/views/pembayaran-parmas/laporan-preview.blade.php
+++ resources/views/pembayaran-parmas/laporan-preview.blade.php
@@ -55,38 +55,38 @@
         <br>
         <b>Dari tanggal {{ \Carbon\Carbon::parse(request()->tanggal_mulai)->format('d-m-Y') }} -
             {{ \Carbon\Carbon::parse(request()->tanggal_selesai)->format('d-m-Y') }}</b><br><br>
-        {{-- <table style="" border="1" cellspacing="0" cellpadding="10" width="100%">
-        <thead>
-            <tr>
-                <th scope="col" style="font-family: sans-serif;">No</th>
-                <th scope="col" style="font-family: sans-serif;">Nama Siswa</th>
-                <th scope="col" style="font-family: sans-serif;">Nisn</th>
-                <th scope="col" style="font-family: sans-serif;">Kelas</th>
-                <th scope="col" style="font-family: sans-serif;">Tanggal Bayar</th>
-                <th scope="col" style="font-family: sans-serif;">Bulan</th>
-                <th scope="col" style="font-family: sans-serif;">Tahun</th>
-                <th scope="col" style="font-family: sans-serif;">Petugas</th>
-                <th scope="col" style="font-family: sans-serif;">Jumlah Bayar</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($pembayaran as $row)
+        <table style="" border="1" cellspacing="0" cellpadding="10" width="100%">
+            <thead>
                 <tr>
-                    <th scope="row" style="font-family: sans-serif;">{{ $loop->iteration }}</th>
-                    <td style="font-family: sans-serif;">{{ $row->siswa->nama_siswa }}</td>
-                    <td style="font-family: sans-serif;">{{ $row->nisn }}</td>
-                    <td style="font-family: sans-serif;">{{ $row->siswa->kelas->nama_kelas }}</td>
-                    <td style="font-family: sans-serif;">
-                        {{ \Carbon\Carbon::parse($row->tanggal_bayar)->format('d-m-Y') }}
-                    </td>
-                    <td style="font-family: sans-serif;">{{ $row->bulan_bayar }}</td>
-                    <td style="font-family: sans-serif;">{{ $row->tahun_bayar }}</td>
-                    <td style="font-family: sans-serif;">{{ $row->petugas->nama_petugas }}</td>
-                    <td style="font-family: sans-serif;">{{ $row->jumlah_bayar }}</td>
+                    <th scope="col" style="font-family: sans-serif;">No</th>
+                    <th scope="col" style="font-family: sans-serif;">Nama Siswa</th>
+                    <th scope="col" style="font-family: sans-serif;">Nisn</th>
+                    <th scope="col" style="font-family: sans-serif;">Kelas</th>
+                    <th scope="col" style="font-family: sans-serif;">Tanggal Bayar</th>
+                    <th scope="col" style="font-family: sans-serif;">Bulan</th>
+                    <th scope="col" style="font-family: sans-serif;">Tahun</th>
+                    <th scope="col" style="font-family: sans-serif;">Petugas</th>
+                    <th scope="col" style="font-family: sans-serif;">Jumlah Bayar</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table> --}}
+            </thead>
+            <tbody>
+                @foreach ($pembayaran as $row)
+                    <tr>
+                        <th scope="row" style="font-family: sans-serif;">{{ $loop->iteration }}</th>
+                        <td style="font-family: sans-serif;">{{ $row->siswa->nama_siswa }}</td>
+                        <td style="font-family: sans-serif;">{{ $row->nisn }}</td>
+                        <td style="font-family: sans-serif;">{{ $row->siswa->kelas->nama_kelas }}</td>
+                        <td style="font-family: sans-serif;">
+                            {{ \Carbon\Carbon::parse($row->tanggal_bayar)->format('d-m-Y') }}
+                        </td>
+                        <td style="font-family: sans-serif;">{{ $row->bulan_bayar }}</td>
+                        <td style="font-family: sans-serif;">{{ $row->tahun_bayar }}</td>
+                        <td style="font-family: sans-serif;">{{ $row->petugas->nama_petugas }}</td>
+                        <td style="font-family: sans-serif;">{{ $row->jumlah_bayar }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </body>
 
