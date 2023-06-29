2003cbb6fe8ce8f08715977acd5f82cf2faa2e16
itdev22 <90917112+itdev22@users.noreply.github.com>
2023-06-29 23:08:47 +0700

    Update PembayaranTagihanController.php

--- app/Http/Controllers/PembayaranTagihanController.php
+++ app/Http/Controllers/PembayaranTagihanController.php
@@ -102,7 +102,6 @@ class PembayaranTagihanController extends Controller
             ->where('status', '=', 'pending')
             ->get();
 
-        dd($pembayaran->count());
         if ($pembayaran->count() == 0) {
 
             if ($request->metode_pembayaran == 'Online') {
