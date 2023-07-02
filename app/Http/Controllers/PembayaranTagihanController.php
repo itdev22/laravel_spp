3996d634780cb8e0c36dc564736d1bc9330d1b7e
dinr19 <dinaromawati1911@gmail.com>
2023-07-02 21:08:04 +0700

    Revert "tagihan siswa"
    This reverts commit 3f617b37285eb9badcb1b195d53bf7daf8f3c5de.

--- app/Http/Controllers/PembayaranTagihanController.php
+++ app/Http/Controllers/PembayaranTagihanController.php
@@ -133,7 +133,7 @@ class PembayaranTagihanController extends Controller
                 }
             }
 
-            return redirect()->route('siswa.history-tagihan.index')->with('success', 'Pembayaran berhasil disimpan! Pembayaran berhasil disimpan! Lanjut Pembayaran Dengan Memilih Link Payment! Cek sesuai tanggal bayar kalian!!');
+            return redirect()->route('siswa.history-tagihan.index')->with('success', 'Pembayaran berhasil disimpan!');
         } else {
             return back()->with('error', 'Pembayaran gagal disimpan!');
         }
